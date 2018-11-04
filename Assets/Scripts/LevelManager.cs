42b2ac396c5fe8c34436b4ee7440c33aa2b6e89b
buns919 <sbehun07+1@gmail.com>
2018-11-03 20:37:03 -0400

    basic working scene nav

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+    public float autoLoadNextLevelTime;
+
+    private void Awake() {
+
+    }
+
+    private void Start() {
+        if (autoLoadNextLevelTime != 0) {
+            Invoke("LoadNextScene", autoLoadNextLevelTime);
+        }
+    }
+
+    public void LoadNextScene() {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+    public void LoadStartScene() {
+        SceneManager.LoadScene("01 Start");
+    }
+
+    public void LoadGameScene() {
+        SceneManager.LoadScene("02 Level_01");
+    }
+
+    public void LoadScene(string levelName) {
+        SceneManager.LoadScene(levelName);
+    }
+
+    public void LoadOptionsScene() {
+        SceneManager.LoadScene("01a Options");
+    }
+
+    public void QuitGame() {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour {
+
+    private AudioSource audioSource;
+
+    public AudioClip[] levelMusicChangeArray;
+
+    private void Awake() {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Use this for initialization
+    private void Start () {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void OnLevelWasLoaded(int level) {
+        AudioClip currentLevelMusic = levelMusicChangeArray[level];
+        Debug.Log("Playing clip: " + currentLevelMusic);
+
+        if (currentLevelMusic) {
+            audioSource.clip = currentLevelMusic;
+            audioSource.loop = true;
+            audioSource.Play();
+        }
+    }
+}
